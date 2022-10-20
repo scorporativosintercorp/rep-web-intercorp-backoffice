dcba14cfbddb8102df67e1657755d9d4291d7005
Huarcaya <phuarcaya@gmail.com>
2022-10-20 18:47:33 -0500

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,13 +1,13 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://modobetaintercorp.azurewebsites.net/api/"',
-  TINY_EDITOR_KEY: '"uqt4tfg89ual3r79xcxqnfwowaujd9jewqlz8izm996jeqgd"',
-  IMAGES_URL: '"https://modobetastorage.blob.core.windows.net/imagenes/"',
-  MEDIA_URL: '"https://modobetastorage.blob.core.windows.net/media/"',
-  ARCHIVOS_URL: '"https://modobetastorage.blob.core.windows.net/archivos/"',
+  API_URL: '"https://web-intercorp-backend-uat.azurewebsites.net/api/"',
+  TINY_EDITOR_KEY: '"c93dm4lwegxjxnp7vk8ektst2aw2uudmsnrguo2mk0tzbw01"',
+  IMAGES_URL: '"https://sawebintercorpuat.blob.core.windows.net/imagenes/"',
+  MEDIA_URL: '"https://sawebintercorpuat.blob.core.windows.net/media/"',
+  ARCHIVOS_URL: '"https://sawebintercorpuat.blob.core.windows.net/archivos/"',
   SSO_APPID: '"1023"',
   SSO_URL: '"https://sso.somosintercorp.net/"',
-  SSO_RETURN_URL: '"https://lively-sand-0fd19270f.azurestaticapps.net/%23/sso/acceso"',
+  SSO_RETURN_URL: '"https://calm-smoke-0e160720f-4.eastus2.1.azurestaticapps.net/%23/sso/acceso"',
   SSO_TOKEN: '"MzI3OUY0QTktN0Q1NC00RjhDLUE5QUYtQjA1RUE2OTI1OTYz"'
 }
